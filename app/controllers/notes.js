95034520ecea798388d04fc5b81ac413d8bac644
Becca <rebecca.vasil@gmail.com>
2015-01-27 11:45:27 -0700

    deleted something important, now everything works

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -4,7 +4,11 @@ export default Ember.ArrayController.extend({
   actions: {
     newNote: function() {
       var body = this.get('noteCopy');
-  },
+
+      var note = this.store.createRecord('note', { body: body });
+      this.set('noteCopy', '');
+      note.save();
+    },
 
   deleteNote: function(id) {
         var self = this;
