281ca0f265a1d746c5b2902a1f06d749cae2dfac
Becca <rebecca.vasil@gmail.com>
2015-01-27 14:24:03 -0700

    user cannot enter in an empty note and main page is being worked on

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -5,16 +5,18 @@ export default Ember.ArrayController.extend({
     newNote: function() {
       var body = this.get('noteCopy');
 
-      var note = this.store.createRecord('note', { body: body });
+      if (body.trim()) {
+        var note = this.store.createRecord('note', { body: body });
+        note.save();
+      }
       this.set('noteCopy', '');
-      note.save();
     },
 
-  deleteNote: function(id) {
-        var self = this;
-        this.store.find('note', id).then(function(note) {
-          note.deleteRecord();
-          note.save();
+    deleteNote: function(id) {
+      var self = this;
+      this.store.find('note', id).then(function(note) {
+        note.deleteRecord();
+        note.save();
         });
       }
     }
