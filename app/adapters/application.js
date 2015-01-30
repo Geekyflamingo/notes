2037ff05e7d9baaa3ad34700f03112d83285b69f
Becca <rebecca.vasil@gmail.com>
2015-01-30 11:22:47 -0700

    playing with ember components

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,6 @@
+import ENV from "../config/environment";
 import DS from "ember-data";
 
-export default DS.LSAdapter.extend({});
+export default DS.ActiveModelAdapter.extend({
+  host: ENV.apiURL
+});
--- config/environment.js
+++ config/environment.js
@@ -12,6 +12,25 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
+  // contentSecurityPolicy: {
+  //     'default-src': "'none'",
+  //     'script-src': "'self' https://cdn.mxpnl.com", // Allow scripts from https://cdn.mxpnl.com
+  //     'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
+  //     'connect-src': "'self' https://api.mixpanel.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
+  //     'img-src': "'self'",
+  //     'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
+  //     'media-src': "'self'"
+  //   },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' localhost:3000",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    },
 
     APP: {
       // Here you can pass flags/options to your application instance
@@ -20,6 +39,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.apiURL = 'http://localhost:3000';
     // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -28,6 +48,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'test') {
+    ENV.apiURL = '';
     // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
@@ -40,7 +61,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.apiURL = 'https://make-me-notes.herokuapp.com/';
   }
 
   return ENV;
